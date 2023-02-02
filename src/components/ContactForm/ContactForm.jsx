101845c649dd3498169d1657f43efbce062148f5
Lina Budkova <linabudkova35@gmail.com>
2023-02-02 18:33:13 +0200

    qweqwe

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,30 @@
+import { Formik, Field, ErrorMessage, Form } from "formik";
+import React from 'react';
+import * as yup from 'yup';
+
+const initialValues = {
+    name: '',
+}
+
+const schema = yup.object().shape({
+    name: yup.string().required(),
+})
+
+export const ContactForm = () => {
+    const handleSubmit = (values, {resetForm}) => {
+        console.log(values);
+        resetForm();
+    }
+
+    return (
+        <Formik initialValues={initialValues} onSubmit = {handleSubmit} validationSchema={schema}>
+            <Form autoComplete="off">
+                <label htmlFor="name">Name
+                    <Field type="text" name="name" />
+                </label>
+                <ErrorMessage name='name'/>
+                <button type="submit">Add contact</button>
+                </Form>
+            </Formik>
+    )
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,9 @@
+export const ContactList = (contacts) => {
+    return (
+        <ul>
+            {contacts.map(({ name, id }) => {
+                return <li key={id}>{name}</li>
+             })}
+        </ul>
+    )
+}
