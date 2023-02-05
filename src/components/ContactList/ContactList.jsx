7db6b190062106b8ace4136a0a85d8edc0242484
Lina Budkova <linabudkova35@gmail.com>
2023-02-05 16:06:57 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,7 +2,7 @@ export const ContactList = ({ contacts, OnDeleteContact  }) => {
 
     return (
         <ul>
-            {contacts.map(({ name, id, number }) => {
+            {contacts.map(({ name, id, number,  }) => {
                 return <li key={id}>{name}: {number}
                     <button type="button" onClick={OnDeleteContact}>Delete</button>
                 </li>
